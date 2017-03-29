cb1c91c3c87d161648333433783381da87f3363b
Jaken Herman <jch053@shsu.edu>
2017-03-28 21:15:39 -0500

    linked LoginActivity with MainActivity

---
+++ BoozeHound-master/BoozeHound1.2/app/src/main/java/com/boozehound/boozehound12/MainMenu.java
@@ -0,0 +1,23 @@
+package com.boozehound.boozehound12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        String[] venues = {"Venue1", "Venue2", "venue3", "venue4", "venue5", "venue6", "venue7",};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, venues);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
+}
+
