4b698c85eecba7be18c14b8325b59e19e64e6b42
jlw106 <jlw106@shsu.edu>
2017-04-26 13:36:17 -0500

    Working Map

--- BoozeHound1.2/app/src/main/java/com/boozehound/boozehound12/MainMenu.java
+++ BoozeHound1.2/app/src/main/java/com/boozehound/boozehound12/MainMenu.java
@@ -2,9 +2,15 @@ package com.boozehound.boozehound12;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -18,6 +24,25 @@ public class MainMenu extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
+
+        Button btn = (Button)findViewById(R.id.button7);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d("maps", "button pressed");
+                loadMapPage();
+            }
+        });
+    }
+
+
+
+    public void loadMapPage(){
+        Intent MapIntent = new Intent(this, MapActivity.class);
+        startActivity(MapIntent);
+        // Intent MapIntent = new Intent(MainMenu.this, MapActivity.class);
+        //MainMenu.this.startActivity(MapIntent);
     }
 }
 
--- BoozeHound1.2/app/src/main/java/com/boozehound/boozehound12/MapActivity.java
+++ BoozeHound1.2/app/src/main/java/com/boozehound/boozehound12/MapActivity.java
@@ -1,26 +1,40 @@
 package com.boozehound.boozehound12;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.graphics.drawable.Drawable;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.support.v4.app.Fragment;
 
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("maps", "page loading");
+
         // Retrieve the content view that renders the map.
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_map);
+        Log.d("maps", "content view set");
+
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-        .findFragmentById(R.id.map);
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-}
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    }
