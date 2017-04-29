d484e543ed724c81b368e2d2b1fcc5b94ae8fc32
jlw106 <jlw106@shsu.edu>
2017-04-29 13:04:04 -0500

    added files from Ryan's repo

--- BoozeHound1.2/app/src/main/java/com/boozehound/boozehound12/MapActivity.java
+++ BoozeHound1.2/app/src/main/java/com/boozehound/boozehound12/MapActivity.java
@@ -1,30 +1,46 @@
 package com.boozehound.boozehound12;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.webkit.WebView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import android.support.v4.app.Fragment;
+import com.kosalgeek.android.json.JsonConverter;
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
-    @Override
+import java.util.*;
+
+
+public class MapActivity extends AppCompatActivity  implements OnMapReadyCallback, AsyncResponse {
+    private ArrayList<GetLocation> locationlist;
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d("maps", "page loading");
 
-        // Retrieve the content view that renders the map.
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        //making AsyncTast reading from web url to get php file
+        PostResponseAsyncTask taskRead = new PostResponseAsyncTask(MapActivity.this, this);
+        taskRead.execute("http://ryandeal.me/getLocation.php");
+
+        /*Button backArrow = (Button) findViewById(R.id.arrowBack);
         Log.d("maps", "content view set");
+
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MapActivity.this, MainMenu.class));
+            }
+        }); */
+
 
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
@@ -33,19 +49,71 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
     @Override
-    public void onMapReady(GoogleMap map) {
-        //run query
-        String type = "map load";
-        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
-        backgroundWorker.execute(type);
-        //int numBars = 5;
-        //for(int i=0; i<numBars; i++) {
-            //addmarkers
-            AddMarker("-95.549686", "30.723162", map, "12th street bar");
-            //set camera
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(30.723526, -95.550777), 14.0f));
-        //}
+    public void processFinish(String s) {
+
+        locationlist = new JsonConverter<GetLocation>().toArrayList(s, GetLocation.class);
+
+     /*   BindDictionary<GetLocation> dict = new BindDictionary<GetLocation>();
+        dict.addStringField(R.id.VenueID, new StringExtractor<Product>(){
+            @Override
+            public String getStringValue(Product product, int position){
+                return "" + product.VenueID;
+            }
+       });
+
+        dict.addStringField(R.id.City, new StringExtractor<Product>() {
+            @Override
+            public String getStringValue(Product product, int position) {
+                return product.City;
+            }
+        });
+        dict.addStringField(R.id.State, new StringExtractor<Product>() {
+            @Override
+            public String getStringValue(Product product, int position) {
+                return product.State;
+            }
+        });
+        dict.addStringField(R.id.ZIP, new StringExtractor<Product>() {
+            @Override
+            public String getStringValue(Product product, int position) {
+                return "" + product.ZIP;
+            }
+        });
+        dict.addStringField(R.id.Longitude, new StringExtractor<GetLocation>() {
+            @Override
+            public String getStringValue(GetLocation location, int position) {
+                return location.Longitude;
+            }
+        });
+        dict.addStringField(R.id.Latitude, new StringExtractor<GetLocation>() {
+            @Override
+            public String getStringValue(GetLocation location, int position) {
+                return location.Latitude;
+            }
+        });*/
+
     }
+    public void onMapReady(GoogleMap map) {
+        //process query results
+        String type = "map load";
+        int numBars = locationlist.size();
+        for(int i=0; i<numBars; i++) {
+            //parse string
+            String loc = locationlist.get(i).toString();
+            String[] parts = loc.split("L");
+            String[] longitparts = parts[1].split("e");
+            String longit = longitparts[2];
+            String[] latitparts = parts[2].split("e");
+            String latit = latitparts[2];
+            Log.d("maps", loc);
+            //add markers
+            AddMarker(longit, latit, map, Integer.toString(i));
+            //AddMarker("-95.549686", "30.723162", map, "12th street bar");
+        }
+        //set camera
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(30.723526, -95.550777), 14.0f));
+    }
+
 
     public void AddMarker(String Long, String Lat, GoogleMap map, String Name){
         Double Longitude = Double.parseDouble(Long);
@@ -54,4 +122,5 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-    }
+}
+
